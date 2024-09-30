043fabedd1a779433287276519d4f75fe63af684
Daksh Panchratna <129274812+daksh56@users.noreply.github.com>
2024-09-30 15:30:07 +0530

    Add files via upload

---
+++ java_connectivity.sql
@@ -0,0 +1,4 @@
+create database minibanking;
+use minibanking;
+create table signup (formno varchar(20), name varchar(20), Father_name varchar(20), dob varchar(20), gender varchar(20),email varchar(20), marital_status varchar(20),address varchar(50), city varchar(20), pincode varchar(20), state varchar(20));
+show tables;
